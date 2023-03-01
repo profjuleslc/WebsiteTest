ec3dc655e22057ce8b6533db661f3b917086d5ca
profjuleslc <126034254+profjuleslc@users.noreply.github.com>
2023-02-28 16:09:18 -0800

    update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   // put setup code here
+  createCanvas(300, 300);
 }
 
 function draw() {
   // put drawing code here
+  background(255);
+  ellipse(20, 20, mouseX, mouseY);
 }
