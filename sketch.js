f72348856ce236eb6d10d8341b2ff0baba1eb89c
profjuleslc <126034254+profjuleslc@users.noreply.github.com>
2023-02-28 16:36:35 -0800

    test subdomain link

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 function setup() {
   // put setup code here
   createCanvas(300, 300);
+  link = text("Click Here to Send", 120, 120);
 }
 
 function draw() {
   // put drawing code here
   background(255);
   ellipse(20, 20, mouseX, mouseY);
+  if(keyPressed == SPACE) {
+  window.open("https://profjuleslc.github.io/WebsiteTest/nextTest/index.html");
+  }
 }
